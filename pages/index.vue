25466176f158bf5649a5355cf7fc0885af32b320
Kamsi-yonna <etiobikamsi@gmail.com>
2023-07-15 23:10:24 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
+    <div
+      class="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-6-8 p-4"
+    ></div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
