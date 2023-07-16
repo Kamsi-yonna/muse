5775012a1f8720a5921090fabc9387196604ee75
Kamsi-yonna <etiobikamsi@gmail.com>
2023-07-16 03:39:42 +0100

    new commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+  app: {
+    head: {
+      title: "Muse",
+      meta: [
+        {
+          name: "description",
+          content: "",
+        },
+      ],
+      link: [{}],
+    },
+  },
 });
